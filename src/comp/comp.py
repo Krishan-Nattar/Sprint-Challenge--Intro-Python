aa4b98924355662dbbb54d69c9ad60b6ca8257e3
Krishan Nattar <krishan.nattar@gmail.com>
2020-01-17 11:21:42 -0500

    tuples complete

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -70,6 +70,7 @@ print(e)
 # inclusive.
 print("Names and ages between 27 and 32:")
 f = []
+[f.append((x.name, x.age)) for x in humans if x.age >= 27 and x.age <=32]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
